d7d0ce4a8fec106a827dae76308c67be0f7aebd2
GabyY1999 <74101166+GabyY1999@users.noreply.github.com>
2023-09-25 23:17:11 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from configuracion import credenciales
 from configuracion import SECRET_KEY  # Importa la clave secreta desde tu archivo de configuración
-from .controllers.auth import auth  # Importa tu Blueprint de autenticación
+from .routes.auth import auth  # Importa tu Blueprint de autenticación
+from .routes.servidor_blueprint import servidor_bp 
+from .routes.canal_blueprint import canal_bp
+from .routes.mensaje_blueprint import mensaje_bp
 
 def create_app():
     """Crea y configura la aplicación de Flask"""
     app = Flask(__name__)
     app.config['SECRET_KEY'] = SECRET_KEY
     app.config.from_object(credenciales)
-
     # Registra tus Blueprints aquí
     app.register_blueprint(auth)
+    app.register_blueprint(servidor_bp)
+    app.register_blueprint(canal_bp)
+    app.register_blueprint(mensaje_bp)
     return app
-
