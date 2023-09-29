2ac31885b12626f6a74a32584725d021baf80e99
GabyY1999 <74101166+GabyY1999@users.noreply.github.com>
2023-09-29 04:53:10 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from configuracion import credenciales
 from configuracion import SECRET_KEY  # Importa la clave secreta desde tu archivo de configuración
-from .routes.auth import auth  # Importa tu Blueprint de autenticación
+from .routes.usuario_blueprint import auth  # Importa tu Blueprint de autenticación
 from .routes.servidor_blueprint import servidor_bp 
 from .routes.canal_blueprint import canal_bp
 from .routes.mensaje_blueprint import mensaje_bp
