57d76b27007b17864c67446a31fa42589786cb64
GabyY1999 <74101166+GabyY1999@users.noreply.github.com>
2023-09-29 04:51:27 -0300

    Update mensaje_blueprint.py

--- app/routes/mensaje_blueprint.py
+++ app/routes/mensaje_blueprint.py
@@ -3,5 +3,5 @@ from ..controllers.mensaje_controller import MensajeController
 
 mensaje_bp = Blueprint('mensaje', __name__)
 
-mensaje_bp.route('/obtener_mensajes_canal/<int:channelId>', methods=['GET'])(MensajeController.obtener_mensajes)
-mensaje_bp.route()()
+mensaje_bp.route('/obtener_mensajes_canal/<int:canalID>', methods=['GET'])(MensajeController.obtener_mensajes)
+mensaje_bp.route('/enviar_mensaje', methods=['POST'])(MensajeController.enviar_mensaje)
