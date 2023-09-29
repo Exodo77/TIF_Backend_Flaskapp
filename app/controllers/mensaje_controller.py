056700ff5ecb11a32eb29045b5f1988953ec1260
GabyY1999 <74101166+GabyY1999@users.noreply.github.com>
2023-09-29 04:43:51 -0300

    Update mensaje_controller.py

--- app/controllers/mensaje_controller.py
+++ app/controllers/mensaje_controller.py
@@ -1,26 +1,41 @@
+# mensaje_controller.py
 from flask import request, jsonify
-from ..models.canal_model import Canal
-
+from ..models.mensaje_model import Mensaje
+from ..models.auth.usuario_model import Usuario
 
 class MensajeController:
+    @classmethod
+    def obtener_mensajes(cls, canalID):
+        print(canalID)
+        print("Llegó hasta obtener mensajes")
+        mensajes=Mensaje.lista_mensaje(int(canalID))
+        if len(mensajes) >0:
+            # Formatear los mensajes en el formato deseado
+            formatted_messages = []
+            for mensaje in mensajes:
+                formatted_message = {
+                    "canalID": mensaje[0],
+                    "autor": Usuario.obtener_userName(mensaje[2]),  # Reemplaza con el nombre del autor si es necesario
+                    "contenido": mensaje[3],  # Reemplaza con el contenido del mensaje
+                    "fecha_envio": mensaje[4].strftime("%Y-%m-%d %H:%M:%S")
+                }
+                formatted_messages.append(formatted_message)
+                print(formatted_messages)
+            return jsonify(formatted_messages),200 # Devolvemos los mensajes como JSON
+        else:
+            lista=[{"canalID": 0, "autor": "servidor", "contenido": "Bienvenido comienza a mandar mensajes", "fecha_envio": "2023-09-25 19:14:02"}]
+            return jsonify(lista),200
     
-    
-    classmethod
-    def obtener_mensajes(cls,chanel_id):
-        print(chanel_id)
-        print("Llego hasta obtener mensajes")
-        try:
-            # Supongamos que aquí conectas a tu base de datos y consultas los mensajes del canal correspondiente
-            # messages = consulta_a_la_base_de_datos(chanel_id)
-        
-            # Por ahora, usemos datos de ejemplo
-            messages = [
-                {"mensaje_id": 1, "autor": "robin", "contenido": "Hola", "fecha_envio": "2023-09-25 19:14:02"},
-                {"mensaje_id": 2, "autor": "alice", "contenido": "Hola de nuevo", "fecha_envio": "2023-09-25 19:15:20"},
-            # Agrega más mensajes si es necesario
-            ]
-
-            return messages
-        except Exception as e:
-        # Manejo de errores
-            return jsonify({'message': 'Error al obtener los mensajes del canal'}), 500
+    @classmethod
+    def enviar_mensaje(cls):
+        print("Llego hasta enviar_mensaje")
+        data = request.json
+        canal_id = data.get('canal_id')
+        texto = data.get('texto')
+        user_id=data.get('user_id')
+        resul=Mensaje.crear_mensaje(canal_id,user_id,texto)
+        print(resul)
+        if resul:
+            return jsonify({'message': 'Mensaje enviado con éxito'}),200
+        else:
+            return jsonify({'message': 'Error al enviar el mensaje'}),500
