b8b7664330a1ccb638bc83593e4751191baa7510
GabyY1999 <74101166+GabyY1999@users.noreply.github.com>
2023-09-29 04:43:18 -0300

    Update canal_controller.py

--- app/controllers/canal_controller.py
+++ app/controllers/canal_controller.py
@@ -1,42 +1,31 @@
 from flask import request, jsonify
 from ..models.canal_model import Canal
-from ..models.servidores_model import Servidor
-
-
 
 class CanalController:
 
     @classmethod
     def obtener_canales_servidor(cls, servidor_id):
+        """OBTIENE LOS CANALES DE LOS SERVIDORES"""
         print("Llego asta obtener canales servidor")
         try:
             canales=Canal.obtener_canales(servidor_id)
-            #print(canales)
-            # Devuelve una lista de canales en formato JSON
-            #canales = [{'nombre': 'Canal 1'}, {'nombre': 'Canal 2'}]  # Reemplaza con los canales reales
+            # Devuelve una respuesta JSON con los canales
             return jsonify(canales), 200
         except Exception as e:
-            # Manejo de errores
+            # Devuelve una respuesta JSON con un mensaje de error si hay un problema
             response = {'message': 'Error al obtener los canales del servidor'}
             return jsonify(response), 500
 
     @classmethod    
     def crear_canal(cls):
+        """CREA UN CANAL"""
         try:
             data = request.json
             nombre_canal = data.get('nombre_canal')
             servidor_id = int(data.get('servidor_id'))#servidor_id lo obtiene como un string
-            #print("DATOS OBTENIDOS PARA CREAR CANAL:", nombre_canal, servidor_id)
-            # Llama a la función para crear el canal
-            resul = Servidor.agregar_canal(servidor_id, nombre_canal)
-            print (resul)
-        #print(resul)
-            #if resul:
+            resul =Canal.crear_canal(servidor_id, nombre_canal)
                 # Devuelve una respuesta JSON exitosa si el canal se crea con éxito
             return jsonify({'message': 'Canal creado con éxito'}), 200
-            #else:
-                # Devuelve una respuesta JSON con un mensaje de error si hay un problema
-            #    return jsonify({'message': 'Error al crear el canal'}), 500
         except Exception as e:
-            # Manejo de errores más genérico
+            # Devuelve una respuesta JSON con un mensaje de error si hay un problema
             return jsonify({'message': 'Error en crear canal'}), 500
